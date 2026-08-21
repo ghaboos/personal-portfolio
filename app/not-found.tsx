4cba13030d9a025bdd4a833dd9baceb86f3c1722
ghaboos <147740858+ghaboos@users.noreply.github.com>
2026-08-21 16:53:38 +0330

    feat: add cinematic 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,2 @@
+import Link from "next/link";
+export default function NotFound(){return <main className="pageMessage"><div><div className="sectionLabel">ERROR / 404</div><h1>Lost.</h1><p>This page doesn't exist in the archive.</p><Link className="button" href="/">Back home ↗</Link></div></main>}
